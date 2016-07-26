e68dfb73397f4e1d193bf0dcc63432678215138b
Fred <fanfanshao@gmail.com>
2016-07-27 00:40:48 +0800

    添加搜索参数变化时局部刷新ajax请求

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,11 +1,54 @@
+require 'date'
+
 class HomepageController < ApplicationController
 
   def index
-    @rand_number = rand(100000)
+
+    wday_array = ['周日','周一','周二','周三','周四','周五','周六']
+
+   # @rand_number = rand(100000)
+
+    @date_array = Array.new
+    future_day = 12
+
+    now = DateTime.now
+    date_str = now.strftime("%Y.%m.%d");
+    date_str += ' ' + wday_array[now.wday]
+    @date_array.push(date_str)
+
+    for i in 2..future_day
+      tomorrow = now.tomorrow
+      date_str = tomorrow.strftime("%Y.%m.%d")
+      date_str += ' ' + wday_array[tomorrow.wday]
+      @date_array.push(date_str)
+      now = tomorrow
+    end
+
+
+
   end
 
+
+  # _search_type: 0 各下拉框搜索， 1 搜索输入框搜索
   def search
+    search_type = params[:_search_type].to_i
+
+    if search_type == 0
+      date = params[:_date_str]
+      province = params[:_province]
+      city = params[:_city]
+      area = params[:_area]
+      @tmp = date + " " + province + " " + city + " " + area
+    else
+      key_str = params[:_key_str]
+      @tmp = key_str
+    end
+
+
+
    @rand_number = rand(10000)
+
+
 
     respond_to do |format|
       format.js
